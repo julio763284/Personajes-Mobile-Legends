417658a0a61359b51a937f5bc1de2262e5ca9618
julio763284 <julio763284@gmail.com>
2025-09-21 15:11:31 -0500

    Mejoras

--- app/lib/presentacion/vistas/Inicio.dart
+++ app/lib/presentacion/vistas/Inicio.dart
@@ -13,17 +13,11 @@ class Inicio extends StatelessWidget {
         child: Container(
           height: 900.0,
           width: 1000.0,
-          decoration : BoxDecoration(
-          image: DecorationImage(image: AssetImage("fondoredimensionado.jpg"), fit: BoxFit.cover),
-          ),
-          child: Column(
-
-            children: [
-              SizedBox(height: 510.0),
-              ElevatedButton(onPressed: (){}, 
-              style: ButtonStyle(backgroundColor: WidgetStateProperty.all<Color>(Colors.blueAccent), ),
-              child: Text("<Presionar>", style: TextStyle(fontSize: 20.0, color: Colors.white),))
-            ],
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("fondoredimensionado.jpg"),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
       ),
